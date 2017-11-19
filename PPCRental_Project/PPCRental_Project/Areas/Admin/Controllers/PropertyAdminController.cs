47ec6da4523cc93457097b35872452156d1ee59c
san <quanlong200315@gmail.com>
2017-11-19 19:24:30 +0700

    update file Image2

---
+++ PPCRental_Project/PPCRental_Project/Areas/Admin/Controllers/PropertyAdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental_Project.Models;
+namespace PPCRental_Project.Areas.Admin.Controllers
+{
+    public class PropertyAdminController : Controller
+    {
+        K21T3_Team1_PPC3129Entities db = new K21T3_Team1_PPC3129Entities();
+        // GET: Admin/PropertyAdmin
+        public ActionResult Index()
+        {
+            var p = db.PROPERTY.OrderByDescending(x => x.ID).ToList();
+            return View(p);
+        }
+    }
+}
